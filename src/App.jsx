6bc93c0633509effdfc097f5ccea9d8fddca1ad0
Darren <darrendsa90@gmail.com>
2025-12-02 10:10:12 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-purple-600">booking App!</h1>
+    </div>
+  )
+}
+
+export default App
